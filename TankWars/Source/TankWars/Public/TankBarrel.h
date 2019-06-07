add66a5fef3598212dd77b3b66940d0d6c5ea678
oO-Blink-Oo <alvarezian23@gmail.com>
2019-06-07 03:00:34 -0700

    Barrel can now move up and down if its going to hit the target

--- TankWars/Source/TankWars/Public/TankBarrel.h
+++ TankWars/Source/TankWars/Public/TankBarrel.h
@@ -20,7 +20,7 @@ public:
 
 private:
 	UPROPERTY(EditAnywhere)
-	float MaxDegreesPerSecond = 20.f; // sensible default
+	float MaxDegreesPerSecond = 5.f; // sensible default
 
 	UPROPERTY(EditAnywhere)
 	float MaxElevationDegrees = 40.f; // sensible default
