d3177212c0bfb897b604add7852cf4ea1d15b242
oO-Blink-Oo <alvarezian23@gmail.com>
2019-06-12 01:24:13 -0700

    made TankMovementComponent BP callable

--- TankWars/Source/TankWars/Public/TankMovementComponent.h
+++ TankWars/Source/TankWars/Public/TankMovementComponent.h
@@ -9,7 +9,7 @@
 class UTankTrack;
 
 /**
- * Responsivle from driving the tank tracks
+ * Responsible for driving the tank tracks
  */
 
 
@@ -25,6 +25,9 @@ public:
 
 	UFUNCTION(BlueprintCallable, Category = Input)
 	void IntendMoveForward(float Throw);
+
+	UFUNCTION(BlueprintCallable, Category = Input)
+	void IntendTurnRight(float Throw);
 
 
 private:
