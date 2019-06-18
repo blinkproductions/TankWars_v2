504c9c14126a84cc2a78ca48191cb21784b8daf5
oO-Blink-Oo <alvarezian23@gmail.com>
2019-06-18 16:06:14 -0700

    added some log outputs

--- TankWars/Source/TankWars/Public/Tank.h
+++ TankWars/Source/TankWars/Public/Tank.h
@@ -37,6 +37,8 @@ private:
 	// Sets default values for this pawn's properties
 	ATank();
 
+	virtual void BeginPlay() override;
+
 	UPROPERTY(EditDefaultsOnly, Category = Firing)
 	float ReloadTimeInSeconds = 3;
 
