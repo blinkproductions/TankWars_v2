0abf2e571c37cf9e05d6d50b2f50a240338068d4
oO-Blink-Oo <alvarezian23@gmail.com>
2019-06-23 01:57:56 -0700

    added an OnHit log on TankTrack.cpp

--- TankWars/Source/TankWars/Public/TankTrack.h
+++ TankWars/Source/TankWars/Public/TankTrack.h
@@ -26,4 +26,9 @@ private:
 	UTankTrack();
 
 	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
+
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
 };
