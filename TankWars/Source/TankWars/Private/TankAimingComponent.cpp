1ab2ceec43ea76ea0f44a109ff032580a2c24aa7
oO-Blink-Oo <alvarezian23@gmail.com>
2019-06-25 02:31:43 -0700

    created an AI Mortar with previously used components created from cpp

--- TankWars/Source/TankWars/Private/TankAimingComponent.cpp
+++ TankWars/Source/TankWars/Private/TankAimingComponent.cpp
@@ -106,11 +106,11 @@ void UTankAimingComponent::MoveBarrelTowards(FVector AimDirection)
 
 	// Always yaw the shortest way
 	Barrel->Elevate(DeltaRotator.Pitch);
-	if ( DeltaRotator.Yaw < 180) 
+	if ( FMath::Abs(DeltaRotator.Yaw) < 180) 
 	{
 		Turret->Rotate(DeltaRotator.Yaw);
 	}
-	else 
+	else // avoid going the long-way around
 	{
 		Turret->Rotate(-DeltaRotator.Yaw);
 	}
